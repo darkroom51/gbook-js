a080ded9194848d09df477715ef1f9278119400b
darkroom51 <darkroom@wp.pl>
2019-04-09 14:24:31 +0200

    refactor

--- src/js/Toastr.js
+++ src/js/Toastr.js
@@ -1,18 +1,18 @@
 class Toastr {
-    constructor() {
-        this.toastr = document.querySelector('.toastr');
-        this.toastr.addEventListener('click', this.closeToastr.bind(this));
-    }
+	constructor() {
+		this.toastr = document.querySelector('.toastr');
+		this.toastr.addEventListener('click', this.closeToastr.bind(this));
+	}
 
-    openToastr(type, message) {
-        this.toastr.classList.add('show', type);
-        this.toastr.innerHTML = `<h2>${message}...</h2>`;
-    }
+	openToastr(type, message) {
+		this.toastr.classList.add('show', type);
+		this.toastr.innerHTML = `<h2>${message}...</h2>`;
+	}
 
-    closeToastr(e) {
-        this.toastr.classList.remove('show', 'loading', 'error');
-        this.toastr.innerHTML = '';
-    }
+	closeToastr(e) {
+		this.toastr.classList.remove('show', 'loading', 'error');
+		this.toastr.innerHTML = '';
+	}
 }
 
 export const toastr = new Toastr();
--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -1,5 +1,5 @@
-export function truncate( str, n ){
-    if (str.length <= n) return str;
-    var subString = str.substr(0, n-1);
-    return subString.substr(0, subString.lastIndexOf(' ')) + "&hellip;";
+export function truncate(str, n) {
+	if (str.length <= n) return str;
+	var subString = str.substr(0, n - 1);
+	return subString.substr(0, subString.lastIndexOf(' ')) + "&hellip;";
 };
